4b0cfe86ebf0286be301fac90319a464a7ea80e3
GiroSan <girosan86@gmail.com>
2023-09-20 20:34:05 -0300

    Inicio desenvolvimento Json JS

---
+++ script.js
@@ -0,0 +1,25 @@
+const objs= [
+    {
+        nome:"Gilson",
+        idade:37,
+        esta_trabalhando: true,
+        hobbies: ["jogar games", "comer", "beber refri"],
+        detalhes_profissao:{
+          profissao: "programador",
+          "empresa": "SpaceX"
+        }
+      },
+      {
+        "nome":"Marcio",
+        "idade":78,
+        "esta_trabalhando": false,
+        "hobbies": ["ler", "passear", "assister tv"],
+        "detalhes_profissao":{
+          "profissao": "programador",
+          "empresa": ""
+        }
+      }
+    ]
+    
+
+console.log(objs)
